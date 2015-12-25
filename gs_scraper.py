8efbb6c02c9ae78f9d8e0bb8ff312a9f6d811748
kluong22 <k.luong22@gmail.com>
2015-12-25 00:12:51 -0800

    Added capabilities for Amazon
    which doesn't really matter since amazon already has this feature....
    Maybe I could do price search?

--- gs_scraper.py
+++ gs_scraper.py
@@ -1,15 +1,26 @@
 from lxml import html
 import requests
-from time import sleep
 
-web_link = 'http://www.gamestop.com/nintendo-3ds/games/fire-emblem-fates-special-edition/126802'
+#easier to hard code, but if I add more than 3 than I'll use classes
+#or if I do multiple links for each one 
 
-page = requests.get(web_link)
+gs_link = 'http://www.gamestop.com/nintendo-3ds/games/fire-emblem-fates-special-edition/126802'
+amazon_link = 'http://www.amazon.com/Fire-Emblem-Fates-Special-Nintendo-3DS/dp/B017W16ZDK/ref=sr_1_1_twi_gam_1?ie=UTF8&qid=1451030490&sr=8-1&keywords=fire+emblem+special+edition'
+
+page = requests.get(gs_link)
 tree = html.fromstring(page.content)
-
 status = tree.xpath('//div[@class="buttonna"]/a/span/text()')
 
 if ('Not' in status[0]):
-    print '\nnope :('
+    print '\nOut of Stock at GS'
 else:
-    print '\nwoo!'
+    print '\nIn stock at GS!'
+    
+page = requests.get(amazon_link)
+tree = html.fromstring(page.content)
+status = tree.xpath('//span[@class ="a-size-medium a-color-price"]/text()')
+
+if ('unavailable' in status[0]):
+    print '\nOut of Stock at Amazon :('
+else:
+    print '\nIn stock at amazon!'
