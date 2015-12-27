7ad9e6713a6c994504b11a8a74258874676b62b2
kluong22 <k.luong22@gmail.com>
2015-12-27 03:44:54 -0800

    Added email/sleep functionalities
    Works with all test cases. Its creepy emailing myself.

--- gs_scraper.py
+++ gs_scraper.py
@@ -1,26 +1,50 @@
-from lxml import html
-import requests
-
 #easier to hard code, but if I add more than 3 than I'll use classes
 #or if I do multiple links for each one 
+from lxml import html
+import requests
+import smtplib
+from time import sleep
 
-gs_link = 'http://www.gamestop.com/nintendo-3ds/games/fire-emblem-fates-special-edition/126802'
-amazon_link = 'http://www.amazon.com/Fire-Emblem-Fates-Special-Nintendo-3DS/dp/B017W16ZDK/ref=sr_1_1_twi_gam_1?ie=UTF8&qid=1451030490&sr=8-1&keywords=fire+emblem+special+edition'
 
-page = requests.get(gs_link)
-tree = html.fromstring(page.content)
-status = tree.xpath('//div[@class="buttonna"]/a/span/text()')
+from_addr = xxx
+from_pass = xxx
+to_addr = xxx
 
-if ('Not' in status[0]):
-    print '\nOut of Stock at GS'
-else:
-    print '\nIn stock at GS!'
-    
-page = requests.get(amazon_link)
-tree = html.fromstring(page.content)
-status = tree.xpath('//span[@class ="a-size-medium a-color-price"]/text()')
+def main():
+    gs_link = 'http://www.gamestop.com/nintendo-3ds/games/fire-emblem-fates-special-edition/126802'
+    amazon_link = 'http://www.amazon.com/Fire-Emblem-Fates-Special-Nintendo-3DS/dp/B017W16ZDK/ref=sr_1_1_twi_gam_1?ie=UTF8&qid=1451030490&sr=8-1&keywords=fire+emblem+special+edition'
+    found = False
+    count = 0
+    #for 31 days, since the game comes out in Feb
+    month_in_hours = 744
+    while (found == False and count < month_in_hours):
+        page = requests.get(gs_link)
+        tree = html.fromstring(page.content)
+        status = tree.xpath('//div[@class="buttonna"]/a/span/text()')
 
-if ('unavailable' in status[0]):
-    print '\nOut of Stock at Amazon :('
-else:
-    print '\nIn stock at amazon!'
+        if ('Not' in status[0]):
+            print '\nOut of Stock at GS'
+        else:
+            send_mail(from_addr, from_pass, to_addr, 'Gamestop')
+            found = True
+        
+        page = requests.get(amazon_link)
+        tree = html.fromstring(page.content)
+        status = tree.xpath('//span[@class ="a-size-medium a-color-price"]/text()')
+
+        if ('unavailable' in status[0]):
+            print '\nOut of Stock at Amazon :('
+        else:
+            sendmail(from_adr, from_pass, to_addr, 'Amazon')
+            found = True
+        if not (found):
+            sleep(3600)
+            count++
+def send_mail(from_addr, from_pass, to_addr, store):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.login(from_addr,from_pass)
+    msg = '\nThe item you are looking for is available at %s' %store
+    server.sendmail(from_addr,to_addr,msg)
+
+if __name__ == '__main__':
+    main()
